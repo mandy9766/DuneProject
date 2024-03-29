6c703874aadd76b470c2aecb0c235031a911dc0d
mandy <mandy9766@gmail.com>
2024-03-29 15:26:08 +0900

    스크롤뷰 고도화

---
+++ DuneProject/Assets/Scripts/SceneScript/ScrollViewController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using TMPro;
+ 
+public class ScrollViewController : MonoBehaviour
+{
+    private ScrollRect scrollRect;
+    public float space = 50f;
+    public GameObject uiPrefab;
+    public List<RectTransform> uiObjects = new List<RectTransform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddNewUiObject()
+    {
+        var newUi = Instantiate(uiPrefab,scrollRect.content).GetComponent<RectTransform>();
+        uiObjects.Add(newUi);
+        
+        float x = scrollRect.transform.localPosition.x + space;
+        for (int i = 0; i<uiObjects.Count; i++)
+        {
+            uiObjects[i].GetChild(1).GetComponent<TextMeshProUGUI>().text = (i+1).ToString();
+
+            if (i%2 != 0)
+                uiObjects[i].anchoredPosition = new Vector2(x,-150f);
+            else
+                uiObjects[i].anchoredPosition = new Vector2(x,150f);
+
+            x += uiObjects[i].sizeDelta.x + space;
+        }
+        scrollRect.content.sizeDelta = new Vector2(x,scrollRect.content.sizeDelta.y);
+    }
+}
